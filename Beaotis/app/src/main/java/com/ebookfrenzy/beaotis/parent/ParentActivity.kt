7191afd2aa37b2f17241c12566bcf254966a8680
YorickMorris <54893119+YorickMorris@users.noreply.github.com>
2020-12-23 15:10:21 +0200

    nav_graph.xml and ParentActivity added
    Projenin layout görünümü ve bağlantılarını içeren nav_graph eklendi. Buna bağlı bağımlılıklar eklendi. Ebeveyn arayüzü için ParentActivity oluşturuldu.

---
+++ Beaotis/app/src/main/java/com/ebookfrenzy/beaotis/parent/ParentActivity.kt
@@ -0,0 +1,12 @@
+package com.ebookfrenzy.beaotis.parent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ebookfrenzy.beaotis.R
+
+class ParentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_parent)
+    }
+}
