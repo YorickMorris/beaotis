8aa7eb0285140ef9dda757b1a0f5fc67232639cd
YorickMorris <54893119+YorickMorris@users.noreply.github.com>
2021-07-05 17:45:52 +0300

    Navigation_graph
    Navigation graph yenilendi.

--- Beaotis/app/src/main/java/com/ebookfrenzy/beaotis/findingobjects/fruits/groupone/SplashFragment5.kt
+++ Beaotis/app/src/main/java/com/ebookfrenzy/beaotis/findingobjects/fruits/groupone/SplashFragment5.kt
@@ -1,5 +1,6 @@
 package com.ebookfrenzy.beaotis.findingobjects.fruits.groupone
 
+import android.media.MediaPlayer
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,13 +9,17 @@ import android.view.ViewGroup
 import com.ebookfrenzy.beaotis.R
 
 class SplashFragment5 : Fragment() {
-
+    private var mPlayer: MediaPlayer? = null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        mPlayer=MediaPlayer.create(activity, R.raw.elmatelaffuz)
+        mPlayer?.start()
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_splas5, container, false)
+
+
     }
 
 
